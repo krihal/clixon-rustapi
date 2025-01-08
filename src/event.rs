71fc668da00b9cf2a818dbe156b373b23fd66534
Kristofer Hallin <kristofer@sunet.se>
2025-01-08 11:13:43 +0000

    Better event handler.

--- src/event.rs
+++ src/event.rs
@@ -1,35 +1,46 @@
 use std::collections::HashMap;
+use std::fmt;
 
-type Callback = Box<dyn Fn()>;
-
-struct EventHandler {
-    listeners: HashMap<String, Vec<Callback>>,
+struct EventHandler<T> {
+    listeners: HashMap<String, Vec<Box<dyn Fn(&T)>>>,
 }
 
-impl EventHandler {
+impl<T> EventHandler<T> {
     fn new() -> Self {
         Self {
             listeners: HashMap::new(),
         }
     }
 
-    fn register(&mut self, event: &str, callback: Callback) {
+    fn register(&mut self, event: &str, callback: Box<dyn Fn(&T)>) {
         self.listeners
             .entry(event.to_string())
             .or_insert(Vec::new())
             .push(callback);
     }
 
-    fn emit(&self, event: &str) {
-        for (event_name, callback) in &self.listeners {
-            if event_name != event && event_name != "*" {
-                continue;
-            }
+    fn unregister(&mut self, event: &str) {
+        self.listeners.remove(event).unwrap();
+    }
 
-            for cb in callback {
-                println!("Firing callback for event '{}'", event);
-                cb();
+    fn emit(&self, event: &str, parameter: &T) {
+        for (event_name, callback) in &self.listeners {
+            if event_name == event || event_name == "*" {
+                for cb in callback {
+                    cb(parameter);
+                }
             }
         }
+    }
+}
+
+struct Data {
+    name: String,
+    value: i32,
+}
+
+impl fmt::Display for Data {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "name={}, value={}", self.name, self.value)
     }
 }
